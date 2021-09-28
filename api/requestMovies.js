cf78d5c6798086a1ba46eeb04f60caa9420e46bd
Andrea Pietronave <andrea.pietronave.97@gmail.com>
2021-09-28 16:53:17 +0200

    Improved ID

--- api/requestMovies.js
+++ api/requestMovies.js
@@ -4,7 +4,6 @@ const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${config
 const baseUrl = `https://api.themoviedb.org/3/search/movie?api_key=${config.TMDB_API}&`;
 
 exports.getMovies = async function (query) {
-  let key = 0;
   const moviesResults = [];
 
   console.log("Requesting genres");
@@ -24,7 +23,6 @@ exports.getMovies = async function (query) {
     });
 
   for(const movie of data.results) {
-    key += 1;
     const genresName = [];
     for(const id of movie.genre_ids) {
       for(const genre of genres.genres) {
@@ -35,7 +33,7 @@ exports.getMovies = async function (query) {
     }
     moviesResults.push({
       genresName,
-      key,
+      id: movie.id,
       originalTitle: movie.original_title,
       overview: movie.overview,
       posterPath: movie.poster_path,
--- api/requestShow.js
+++ api/requestShow.js
@@ -4,7 +4,6 @@ const genreUrl = `https://api.themoviedb.org/3/genre/tv/list?api_key=${config.TM
 const baseUrl = `https://api.themoviedb.org/3/search/tv?api_key=${config.TMDB_API}&`;
 
 exports.getShows = async function (query) {
-  let key = 0;
   const showsResults = [];
 
   console.log("Requesting genres");
@@ -24,7 +23,6 @@ exports.getShows = async function (query) {
     });
 
   for(const show of data.results) {
-    key += 1;
     const genresName = [];
     for(const id of show.genre_ids) {
       for(const genre of genres.genres) {
@@ -35,7 +33,7 @@ exports.getShows = async function (query) {
     }
     showsResults.push({
       genresName,
-      key,
+      id: show.id,
       originalTitle: show.name,
       overview: show.overview,
       posterPath: show.poster_path,
